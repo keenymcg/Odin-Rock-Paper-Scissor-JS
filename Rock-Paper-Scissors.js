24482036312cb6a62601ab26de190d98d0153653
KEENAN MCGUCKIN <118635604+keenymcg@users.noreply.github.com>
2023-02-16 15:06:13 -0800

    Start over

--- Rock-Paper-Scissors.js
+++ Rock-Paper-Scissors.js
@@ -1,11 +1,16 @@
+/* getComputerChoice returns a random index of the R/P/S array */
 function getComputerChoice() {
     let game_options = ["rock", "paper", "scissors"];
-    return Math.floor(Math.random() * game_options.length);
+    switch 
 }
 
+/* variables to set initial zero'd-out point values */
 let pointTracker_computer = 0
 let pointTracker_human = 0
 
+
+/* playRound sets all the conditional responses to each round
+TO DO: ADD POINT TRACKING? */
 function playRound (playerSelection, computerSelection) {
     if (playerSelection === "rock" && computerSelection === 1) {
         return "You lose! Paper beats Rock";
@@ -14,29 +19,37 @@ function playRound (playerSelection, computerSelection) {
     } else if (playerSelection === "scissors" && computerSelection === 0) {
         return "You lose! Rock beats Scissors"
     } else if (playerSelection === "scissors" && computerSelection === 2 || 
-    playerSelection === "rock" && computerSelection === 0 || 
-    playerSelection === "paper" && computerSelection === 1) {
+                playerSelection === "rock" && computerSelection === 0 || 
+                playerSelection === "paper" && computerSelection === 1) {
         return "It's a tie"
     } else {
         return "You win!"
     }
 }
 
+/* Calls the playRound function inside in order to assign points, but something
+is not working. 
+It checks the strings, but even if the human wins, it says the computer gets a point */
 function game() {
-    playRound();
-        if ("You lose! Rock beats Scissors" ||
-        "You lose! Scissors beats Paper" ||
-        "You lose! Paper beats Rock") {
-            pointTracker_computer += 1;
-        } else if ("You win!") {
-            pointTracker_human += 1;
-        }
+    for (let rounds = 0; rounds < 5; rounds++)
+        playRound();
+            if ("You lose! Rock beats Scissors" ||
+            "You lose! Scissors beats Paper" ||
+            "You lose! Paper beats Rock") {
+                pointTracker_computer += 1;
+            } else if ("You win!") {
+                pointTracker_human += 1;
+    
+    console.log("That's 5 rounds!")        }
+    if (pointTracker_computer < pointTracker_human) {
+        console.log("You win! The Computer loses!");
+    } else {
+        console.log("You lose! The Computer wins!");
+    }
 }
 
 /* make a prompt that takes the user's input, makes it lowercase, 
 and assigns that string a variable
 then make an exception that says "Not a valid input"*/
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(computerSelection); /* don't know why changing this console.log to the getComputerChoice function doesn't */
-console.log(playRound(playerSelection, computerSelection));
+
+console.log(game(playRound(prompt("Rock, Paper, or Scissors? --> "), getComputerChoice)));
