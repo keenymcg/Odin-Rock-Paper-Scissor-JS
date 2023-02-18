d3bbc99255459d5c7f131a7632946653d1dd9245
KEENAN MCGUCKIN <118635604+keenymcg@users.noreply.github.com>
2023-02-17 17:43:27 -0800

    Add pseudo-code for all pieces

--- Rock-Paper-Scissors.js
+++ Rock-Paper-Scissors.js
@@ -1,14 +1,34 @@
 const options = ["ROCK", "PAPER", "SCISSORS"]
 
-// GENERATE RANDOM ROCK, PAPER, OR SCISSORS
 function getComputerChoice() {
-    const randomNumber = options[Math.floor(Math.random() * options.length)];
-    return randomNumber
+    const randOption = options[Math.floor(Math.random() * options.length)]; // length method doesn't start from index 0...
+    // ...therefore there's an equal chance for each option
+    // putting this math in square brackets, prefixed by the "options" var, brings back the string (since it's being indexed)
+    return randOption
     }
 
-function checkWinner(playerChoice, computerChoice) {
-    if (playerChoice === computerChoice) {
-        return "Tie"
+function getPlayerChoice() { // We want to return the player's choice only if certain conditions are true
+    let validatedInput = false; 
+    while(validatedInput == false) {
+        const choice = prompt("Rock Paper Scissors"); // store prompt in "choice" variable because...
+        // we can't check if the user's input is empty without storing the prompt in a variable
+        if (choice == null) {
+            continue // all of this first section in the while loop checks for no input, and if no input is given, it restarts While loop from here
+        }
+        const choiceInUpper = choice.toUpperCase(); // takes the "choice" variable from above...
+        // ...and changes it to the case of the global "options" variable
+        if (options.includes(choiceInUpper)) { // uses ".includes" method on options-array variable to see if it contains the player choice
+            // and if the player choice is something like "poo" or some other invalid input, the condition remains false, and we start the while loop again
+            validatedInput = true;
+            return choiceInUpper; // the result of this function can now only be one of the "options" choices
+        }
+    }
+}
+
+function checkWinner(playerChoice, computerChoice) { // goal of function is only to return a string of the winner
+    // need to pass choices are parameters and then compare them
+    if (playerChoice === computerChoice) { 
+        return "Tie" // start with "Tie" because it's simple
     } 
     else if (
         (playerChoice === "ROCK" && computerChoice === "PAPER") || // Loser conditionals
@@ -16,19 +36,16 @@ function checkWinner(playerChoice, computerChoice) {
         (playerChoice === "SCISSORS" && computerChoice === "ROCK")
     ) {
         return "Computer";
-        // compScore++
-        // console.log(compScore) // SCORE SYSTEM WORKS, resets when testing single round
     } 
     else { 
-        return "Player";
-        // playerScore++
-        // console.log(playerScore)
+        return "Player"; // no need to define the winner conditionals because they are the only options left
     }
 }   
 
-function playRound(playerChoice, computerChoice) {
-    const result = checkWinner(playerChoice, computerChoice);
-    if (result == "Tie") {
+function playRound(playerChoice, computerChoice) { // We pass the player/comp choices as parameters for one reason: 
+    // ...to paste those string values into the winner-declaration "result" string
+    const result = checkWinner(playerChoice, computerChoice); // We pass them as parameters again in checkWinner to store the value in "result" var
+    if (result == "Tie") { 
         return "It's a tie!"
     }
     else if (result == "Player") {
@@ -39,38 +56,23 @@ function playRound(playerChoice, computerChoice) {
     }
 }
 
-function getPlayerChoice() {
-    let validatedInput = false;
-    while(validatedInput == false) {
-        const choice = prompt("Rock Paper Scissors");
-        if (choice == null) {
-            continue
-        }
-        const choiceInUpper = choice.toUpperCase();
-        if (options.includes(choiceInUpper)) {
-            validatedInput = true;
-            return choiceInUpper;
-        }
-    }
-}
-
-function game() {
-    let scorePlayer = 0;
+function game() { // THE REST OF OUR HELPER FUNCTIONS CULMINATE INSIDE THIS FUNCTION such that to play the game we only have to call this function
+    let scorePlayer = 0; 
     let scoreComputer = 0;
-    console.log("Welcome!")
+    console.log("Welcome!") // announces the game before the 5-round loop starts
     for (let i = 0; i < 5; i++) {
-        const playerChoice = getPlayerChoice();
+        const playerChoice = getPlayerChoice(); // getting the player & comp choices are inside of the for loop because they need to change with each round
         const computerChoice = getComputerChoice();
-        console.log(playRound(playerChoice, computerChoice));
-        console.log("--------");
-        if (checkWinner(playerChoice, computerChoice) == "Player") {
+        console.log(playRound(playerChoice, computerChoice)); // we pass the player/computer choices into playRound, which initiates the checkWinner func...
+        if (checkWinner(playerChoice, computerChoice) == "Player") { // the round-winner is announced, and then that info is used to tally the score
             scorePlayer++;
         } 
         else if (checkWinner(playerChoice, computerChoice) == "Computer") {
             scoreComputer++;
         }
+        console.log("--------"); // logs a line to delineate the rounds
     }
-    console.log("Game Over")
+    console.log("Game Over") // loop breaks after 5 rounds
     if (scorePlayer > scoreComputer) {
         console.log("Player was the winner")
     }
